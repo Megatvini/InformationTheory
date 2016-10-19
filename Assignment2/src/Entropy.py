d5d28885eb80678a6befb5f32419dd385addd4c3
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2016-10-20 02:17:27 +0400

    Implement second task

--- Assignment2/src/Entropy.py
+++ Assignment2/src/Entropy.py
@@ -4,26 +4,23 @@ from Distrib import read_distribution, split_grams,\
     get_distribution_list, get_bi_gram_letters, get_one_gram_letters
 
 
-def write_entropy_gram(gram_counts, letters, out):
+def calculate_entropy_gram(gram_counts, letters, out):
     distribution_list = get_distribution_list(gram_counts, letters)
     entropy_list = [x*log2(x) if x > 0 else 0 for x in distribution_list]
     entropy = -sum(entropy_list)
-    out.write(precision_str_from_float(entropy))
-
-
-def write_conditional_entropy(one_gram, two_grams, out):
-    pass
+    return entropy
 
 
 def write_entropy(letter_distribution, out):
     one_gram_counts, two_gram_counts = split_grams(letter_distribution)
     all_letters = get_one_gram_letters()
-    write_entropy_gram(one_gram_counts, all_letters, out)
-    out.write('\n')
+    one_gram_entropy = calculate_entropy_gram(one_gram_counts, all_letters, out)
+    out.write(precision_str_from_float(one_gram_entropy) + '\n')
     all_bi_gram_letters = get_bi_gram_letters(all_letters)
-    write_entropy_gram(two_gram_counts, all_bi_gram_letters, out)
-    out.write('\n')
-    write_conditional_entropy(one_gram_counts, two_gram_counts, out)
+    bi_gram_entropy = calculate_entropy_gram(two_gram_counts, all_bi_gram_letters, out)
+    out.write(precision_str_from_float(bi_gram_entropy) + '\n')
+    conditional_entropy = bi_gram_entropy - one_gram_entropy
+    out.write(precision_str_from_float(conditional_entropy))
 
 
 def write_entropy_to_file(letter_distribution, output_file):
